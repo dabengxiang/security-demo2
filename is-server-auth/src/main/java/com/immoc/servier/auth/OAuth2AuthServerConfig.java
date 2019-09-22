e4830606eea37961329d8924b13eb046c29250f0
cong <836732982@qq,com>
2019-09-22 22:34:12 +0800

    认证服务器完成

---
+++ is-server-auth/src/main/java/com/immoc/servier/auth/OAuth2AuthServerConfig.java
@@ -0,0 +1,54 @@
+package com.immoc.servier.auth;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+
+
+
+
+
+    //先配置客户端
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        //直接冲内存里面读
+        clients.inMemory().withClient("order-app").secret(passwordEncoder.encode("123456")).scopes("read","write").resourceIds("order-app").authorizedGrantTypes("password");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("isAuthenticated()");
+    }
+
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+
+    }
+}
+
+
+
+
---
+++ is-server-auth/src/main/java/com/immoc/servier/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.immoc.servier.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsServiceImpl  implements UserDetailsService {
+
+
+
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return User.withUsername(username).password(passwordEncoder
+                .encode("123456")).authorities("ROLE_ADMIN").build();
+    }
+}
