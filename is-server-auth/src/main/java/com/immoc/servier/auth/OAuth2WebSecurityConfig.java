4e3dafec0e46ccf4e415573e29fdc0295a2901e4
cong <836732982@qq,com>
2019-10-24 00:10:38 +0800

    自定义登陆页面

--- is-server-auth/src/main/java/com/immoc/servier/auth/OAuth2WebSecurityConfig.java
+++ is-server-auth/src/main/java/com/immoc/servier/auth/OAuth2WebSecurityConfig.java
@@ -8,10 +8,17 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -55,7 +62,14 @@ public class OAuth2WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic()
-        .and().logout().logoutSuccessHandler(logoutSuccessHandler);
+        http.formLogin().loginPage("/imooc-signIn.html").loginProcessingUrl("/login")
+                .and().logout().logoutSuccessHandler(logoutSuccessHandler)
+                .and().authorizeRequests().antMatchers("/imooc-signIn.html").permitAll().anyRequest().authenticated()
+                .and().csrf().disable();
+//        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic()
+//                .and().logout().logoutSuccessHandler(logoutSuccessHandler);
+
+
+
     }
 }
